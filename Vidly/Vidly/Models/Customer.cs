1a385c69114167cf726ca30a2642b76293811da5
Dinescu Alexandru-Catalin <alexdinescu888@gmail.com>
2020-11-06 02:37:38 +0200

    Added MembershipType Model

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -13,5 +13,9 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public String Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+
     }
 }
