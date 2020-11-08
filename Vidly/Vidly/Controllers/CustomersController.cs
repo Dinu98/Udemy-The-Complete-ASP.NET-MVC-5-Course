1e41181621664589d00b351258f66130d6058323
Dinescu Alexandru-Catalin <alexdinescu888@gmail.com>
2020-11-09 00:03:18 +0200

    Validation Summary

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -57,6 +57,7 @@ namespace Vidly.Controllers
 
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
