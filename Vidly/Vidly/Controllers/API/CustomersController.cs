88bae19ac61c990b2489957be9230f302a32d073
Dinescu Alexandru-Catalin <alexdinescu888@gmail.com>
2020-11-09 02:12:46 +0200

    Built api

---
+++ Vidly/Vidly/Controllers/API/CustomersController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+        private Dbctx db;
+        public CustomersController()
+        {
+            db = new Dbctx();
+        }
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            var customer = db.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            db.Customers.Add(customer);
+            db.SaveChanges();
+
+            return customer;
+        }
+
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = db.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            customerInDb.Birthday = customer.Birthday;
+            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            customerInDb.Name = customer.Name;
+
+            db.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = db.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            db.Customers.Remove(customerInDb);
+
+            db.SaveChanges();
+        }
+
+
+    }
+}
